e1683a812336589ff642374a203afb50244f408e
dssp100 <dssp@freenet.de>
2017-01-02 20:33:14 -0400

    Finalised app using JSON

--- bridge.h
+++ bridge.h
@@ -1,14 +1,14 @@
 //
-//  bridge h.h
+//  bridge.h
 //  Web Service Example
 //
 //  Created by Daniel Springer on 02/01/2017.
 //  Copyright © 2017 Daniel Springer. All rights reserved.
 //
 
-#ifndef bridge_h_h
-#define bridge_h_h
+#ifndef bridge_h
+#define bridge_h
 #import "AFNetworking/AFNetworking.h"
 
-#endif /* bridge_h_h */
+#endif /* bridge_h */
 
